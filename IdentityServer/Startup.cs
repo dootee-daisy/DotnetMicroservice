1a440f59d7dc0ad9712419be6e5ec5165334c469
dootee-daisy <nhq.huy102@gmail.com>
2024-05-22 22:51:40 +0700

    update authorization

---
+++ IdentityServer/Startup.cs
@@ -0,0 +1,47 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityServer() // cấu hình identityserver cho ứng dụng, dẩm bảo rằng dịch vụ cần thiết cho identityServer được thêm vào DI(Dêpndency Injection)
+                .AddInMemoryClients(Config.Clients)  // thêm danh sách clients vào cấu hình: web, mobile, desktop
+                .AddInMemoryApiScopes(Config.ApiScopes)
+                .AddDeveloperSigningCredential(); // cung cấp chứng chỉ ký dùng cho môi trường phát triển
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseIdentityServer();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
